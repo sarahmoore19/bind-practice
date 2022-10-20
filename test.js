2dc146a79714fa1c37b5f99cdf31ef5d8d7fe6e2
sarahmoore <sarahnmoore19@gmail.com>
2022-10-20 14:05:10 -0400

    used bind to call seTimeout on class instance methods

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-// Your code here
+const Employee = require("./employee")
+
+const john = new Employee("John Wick", "Dog Lover")
+const henry = new Employee("Henry Wick", "Cat Lover")
+
+
+
+const sayName = john.sayName.bind(henry);
+
+setTimeout(sayName, 2000);
+
+const sayOcc = henry.sayOccupation.bind(john);
+
+setTimeout(sayOcc, 3000);
